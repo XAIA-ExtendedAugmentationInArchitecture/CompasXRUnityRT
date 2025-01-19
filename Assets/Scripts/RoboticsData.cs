660758e8432a8ef788d72f97e28b54a5eae9070f
Joseph Kenny <jckenny59@gmail.com>
2025-01-19 17:32:30 -0500

    add robotics data classes for parsing trajectories

---
+++ Assets/Scripts/RoboticsData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompasXR.Robots.Data
+{
+
+    public class Trajectory
+    {
+
+    }
+
+    public class Configuration
+    {
+
+    }
+
+}
